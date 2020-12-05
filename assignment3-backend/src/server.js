0c0d3af79f722406f9060f70634139d75cd4729a
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-12-05 18:39:33 -0500

    add mongoDBEndpoint back

--- assignment3-backend/src/server.js
+++ assignment3-backend/src/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const mongoDBEndpoint = process.env.MONGODB_URI || 'mongodb+srv://mwerzanski:Neworleans17!@webdev.7x1yt.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const mongoDBEndpoint = process.env.MONGODB_URI || 'mongodb://127.0.0.1/collection_name';
 mongoose.connect(mongoDBEndpoint, { useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Error connecting to mongo db'));
@@ -22,7 +22,7 @@ app.use(cors());
 
 app.use('/api/shortener', URLshortenerRouter);
 
-const port = 'mongodb+srv://mwerzanski:Neworleans17!@webdev.7x1yt.mongodb.net/<dbname>?retryWrites=true&w=majority' || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("starting at" + port);
 })
